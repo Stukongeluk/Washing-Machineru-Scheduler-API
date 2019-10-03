01dbae733606a69d35eed7b14f32b58cb6478169
StukOngeluk <kientnguyen@hotmail.com>
2019-10-03 20:22:27 +0200

    removed unneeded imports

--- src/test/java/washing/machineru/scheduler/api/scheduler/SchedulerServiceTest.java
+++ src/test/java/washing/machineru/scheduler/api/scheduler/SchedulerServiceTest.java
@@ -2,13 +2,10 @@ package washing.machineru.scheduler.api.scheduler;
 
 import io.micronaut.test.annotation.MicronautTest;
 import io.micronaut.test.annotation.MockBean;
-import io.reactivex.Scheduler;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import javax.inject.Inject;
-import javax.swing.text.html.Option;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
